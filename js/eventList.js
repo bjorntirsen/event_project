76d9c270c6c13dc1ff32592201c5d86af9c8b60c
Björn Tirsén <bjorntirsen@gmail.com>
2020-10-22 14:18:37 +0200

    Moved methods to eventList

--- js/eventList.js
+++ js/eventList.js
@@ -1,5 +1,35 @@
 class EventList {
   constructor() {
     this.eventArray = [];
+    if (localStorage.getItem("data") == null) {
+      this.initializeFiveEvents();
+    }   
+  }
+
+  //Creates five "default" events
+  initializeFiveEvents() {
+    let event1 = new Event("Celine Dion", "210613", "Concert", "Friends Arena Stockholm", "pictures/celine.jpg");
+    this.eventArray.push(event1);
+    let event2 = new Event("Ozzy Ozbourne", "201205", "Concert", "Friends Arena Stockholm", "pictures/ozzy.jpg");
+    this.eventArray.push(event2);
+    let event3 = new Event("Lars Winnerbäck", "210709", "Concert", "Sofiero Helsingborg", "pictures/lars.jpg");
+    this.eventArray.push(event3);
+    let event4 = new Event("Spökvandring i Gamla Stan", "210613", "Arts & Theater", "Järntorget Stockholm", "pictures/ghost.jpg");
+    this.eventArray.push(event4);
+    let event5 = new Event("House of Comedy", "201020", "Arts & Theater", "Älghjärtat Stockholm", "pictures/comedy.jpg");
+    this.eventArray.push(event5);
+    console.log(this.eventArray);
+    this.updateToLocalStorage();
+  }
+
+  
+  updateToLocalStorage() {
+    let dataStringForm = JSON.stringify(this.eventArray);
+    localStorage.setItem("data", dataStringForm);
+  }
+
+  updateFromLocalStorage() {
+    let dataObjectForm = JSON.parse(localStorage.getItem("data"));
+    this.eventArray = dataObjectForm;
   }
 }
--- js/interface.js
+++ js/interface.js
@@ -1,38 +1,10 @@
 class Interface {
   constructor() {
-    this.eventList = new EventList();
-    if (localStorage.getItem("data") == null) {
-      this.initializeFiveEvents();
-    }      
-  }
-  //Creates five "default" events
-  initializeFiveEvents() {
-    let event1 = new Event("Celine Dion", "210613", "Concert", "Friends Arena Stockholm", "pictures/celine.jpg");
-    this.eventList.eventArray.push(event1);
-    let event2 = new Event("Ozzy Ozbourne", "201205", "Concert", "Friends Arena Stockholm", "pictures/ozzy.jpg");
-    this.eventList.eventArray.push(event2);
-    let event3 = new Event("Lars Winnerbäck", "210709", "Concert", "Sofiero Helsingborg", "pictures/lars.jpg");
-    this.eventList.eventArray.push(event3);
-    let event4 = new Event("Spökvandring i Gamla Stan", "210613", "Arts & Theater", "Järntorget Stockholm", "pictures/ghost.jpg");
-    this.eventList.eventArray.push(event4);
-    let event5 = new Event("House of Comedy", "201020", "Arts & Theater", "Älghjärtat Stockholm", "pictures/comedy.jpg");
-    this.eventList.eventArray.push(event5);
-    console.log(this.eventList.eventArray);
-    this.updateToLocalStorage();
-  }
-
-  updateToLocalStorage() {
-    let dataStringForm = JSON.stringify(this.eventList.eventArray);
-    localStorage.setItem("data", dataStringForm);
-  }
-
-  updateFromLocalStorage() {
-    let dataObjectForm = JSON.parse(localStorage.getItem("data"));
-    this.eventList.eventArray = dataObjectForm;
+    this.eventList = new EventList(this);   
   }
 
   displayEventsOnEventList() {
-    this.updateFromLocalStorage();
+    this.eventList.updateFromLocalStorage();
     for (let i = 0; i < this.eventList.eventArray.length; i++) {
       this.eventDiv(this.eventList.eventArray[i])
     }
