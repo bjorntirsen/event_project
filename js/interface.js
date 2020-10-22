bfc4748cf8b6523c76aa5b4f88e4f62ed623f4b9
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2020-10-22 11:31:13 +0200

    eventDisplay metod

--- js/interface.js
+++ js/interface.js
@@ -34,13 +34,42 @@ class Interface {
   displayEventsOnEventList() {
     this.updateFromLocalStorage();
     let mainContent = document.getElementById("mainContainer");
-
     let div = document.createElement("div");
-    div.innerHTML = "TESTAR";
+    div.innerHTML = ""
     div.setAttribute("id", "eventDiv");
     mainContent.appendChild(div);
+    this.eventDiv(this.eventList.eventArray[0])
   }
 
+  eventDiv(event){
+    let div = document.getElementById("eventDiv")
+    let eventDiv = document.createElement("div"); //img
+    eventDiv.classList.add("eventDiv");
+    let img = document.createElement("img");
+    img.setAttribute("src", event.img);
+    div.appendChild(img)
+
+    let eventInfoDiv = document.createElement("div");
+    eventInfoDiv.classList.add("eventDiv");
+    let nameP = document.createElement("p");
+    nameP.textContent = event.name
+    eventInfoDiv.appendChild(nameP)
+    div.appendChild(eventInfoDiv)
+
+    let locationP = document.createElement("p");
+    locationP.textContent = event.location
+    eventInfoDiv.appendChild(locationP)
+
+    let dateP = document.createElement("p");
+    dateP.textContent = event.date
+    eventInfoDiv.appendChild(dateP)
+
+    let genreP = document.createElement("p");
+    genreP.textContent = event.genre
+    eventInfoDiv.appendChild(genreP)
+
+
+  }
 
 }
 
