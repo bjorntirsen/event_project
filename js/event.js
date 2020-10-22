7e38ff985c9ad387214096f82e407ba86d6e3b39
Björn Tirsén <bjorntirsen@gmail.com>
2020-10-22 09:42:41 +0200

    InitializeFiveEvents method

--- js/event.js
+++ js/event.js
@@ -1,10 +1,11 @@
 class Event {
-  constructor(eventName, date, genre, location, img) {
+  constructor(eventName = "", date = "", genre = "", location = "", img = "") {
     this.name = eventName;
     this.date = date;
     this.genre = genre;
     this.location = location;
     this.img = img;
     console.log('Created event: ' + this.name);
+    console.log(this);
   }
 }
--- js/interface.js
+++ js/interface.js
@@ -3,12 +3,21 @@ class Interface {
     this.eventList = new EventList();
     this.initializeFiveEvents();
   }
+  //Creates five "default" events
   initializeFiveEvents() {
-    let event = {
-      time: '',
-      Place: '',
-    };
+    let event1 = new Event("Celine Dion", "210613", "Concert", "Friends Arena Stockholm", "pictures/celine.jpg");
+    this.eventList.eventArray.push(event1);
+    let event2 = new Event("Ozzy Ozbourne", "201205", "Concert", "Friends Arena Stockholm", "pictures/ozzy.jpg");
+    this.eventList.eventArray.push(event2);
+    let event3 = new Event("Lars Winnerbäck", "210709", "Concert", "Sofiero Helsingborg", "pictures/lars.jpg");
+    this.eventList.eventArray.push(event3);
+    let event4 = new Event("Spökvandring i Gamla Stan", "210613", "Arts & Theater", "Järntorget Stockholm", "pictures/ghost.jpg");
+    this.eventList.eventArray.push(event4);
+    let event5 = new Event("House of Comedy", "201020", "Arts & Theater", "Älghjärtat Stockholm", "pictures/comedy.jpg");
+    this.eventList.eventArray.push(event5);
+    console.log(this.eventList.eventArray);
   }
+  
 }
 
 //Kolla i local-storage om det finns något object
